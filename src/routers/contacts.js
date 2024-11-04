8a3f0b2b9e77ea9ce11c14c1635a86d66775e737
InnaUsenko <innausenko8@gmail.com>
2024-11-04 21:32:14 +0200

    mv routers

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,5 +1,48 @@
 import { Router } from 'express';
+import { getAllContacts, getContactById } from '../services/contacts.js';
+import { isObjectIdOrHexString } from 'mongoose';
 
 const router = Router();
+
+//GET contacts
+router.get('/contacts', async (req, res) => {
+  const contacts = await getAllContacts();
+
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: contacts,
+  });
+});
+
+//GET contact by ID
+router.get('/contacts/:contactId', async (req, res) => {
+  const { contactId } = req.params;
+
+  //Відповідь, якщо формат/тип ID невірний
+  if (!isObjectIdOrHexString(contactId)) {
+    res.status(400).json({
+      message: 'Invalid ID type',
+    });
+    return;
+  }
+
+  const contact = await getContactById(contactId);
+
+  // Відповідь, якщо контакт не знайдено
+  if (!contact) {
+    res.status(404).json({
+      message: 'Contact not found',
+    });
+    return;
+  }
+
+  // Відповідь, якщо контакт знайдено
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: contact,
+  });
+});
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,8 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import contactsRouter from './routers/contacts.js';
 import { env } from './utils/env.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
-import { isObjectIdOrHexString } from 'mongoose';
 
 // Читаємо змінну оточення PORT
 const PORT = Number(env('PORT', '3000'));
@@ -28,46 +27,8 @@ export const setupServer = () => {
     });
   });
 
-  //GET contacts
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
-
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
-
-  //GET contact by ID
-  app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
-
-    //Відповідь, якщо формат/тип ID невірний
-    if (!isObjectIdOrHexString(contactId)) {
-      res.status(400).json({
-        message: 'Invalid ID type',
-      });
-      return;
-    }
-
-    const contact = await getContactById(contactId);
-
-    // Відповідь, якщо контакт не знайдено
-    if (!contact) {
-      res.status(404).json({
-        message: 'Contact not found',
-      });
-      return;
-    }
-
-    // Відповідь, якщо контакт знайдено
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contact,
-    });
-  });
+  // Додаємо роутер до app як middleware
+  app.use(contactsRouter);
 
   app.use('*', (req, res, next) => {
     res.status(404).json({
