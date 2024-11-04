1cd1630843a32da35a842b8c3c8f708948d06dcc
InnaUsenko <innausenko8@gmail.com>
2024-11-04 14:16:48 +0200

    add services layer and some entrypoints on controller

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
+import { isObjectIdOrHexString } from 'mongoose';
 
 // Читаємо змінну оточення PORT
 const PORT = Number(env('PORT', '3000'));
@@ -21,8 +23,49 @@ export const setupServer = () => {
   );
 
   app.get('/', (req, res) => {
-    res.json({
+    res.status(200).json({
       message: 'Hello worm!',
+    });
+  });
+
+  //GET contacts
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
+
+  //GET contact by ID
+  app.get('/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
+
+    //Відповідь, якщо формат/тип ID невірний
+    if (!isObjectIdOrHexString(contactId)) {
+      res.status(400).json({
+        message: 'Invalid ID type',
+      });
+      return;
+    }
+
+    const contact = await getContactById(contactId);
+
+    // Відповідь, якщо контакт не знайдено
+    if (!contact) {
+      res.status(404).json({
+        message: 'Contact not found',
+      });
+      return;
+    }
+
+    // Відповідь, якщо контакт знайдено
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contact,
     });
   });
 
